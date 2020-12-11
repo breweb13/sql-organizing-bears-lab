2186b94508ab0fd8aa9de969ae57c1dcb1736482
Breanna <breanna@breannawebster.com>
2020-12-11 12:33:24 -0500

    commit tree 618a3a73f3f676343eb7b0eb5f14a8f3af39e35a

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Choclate',5,'M','brown','happy','true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 3, 'M','brown', 'funny', 'false');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6,  'F','brown', 'goofy', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 8, 'M','brown', 'mean', 'false');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 1, 'F','brown', 'nice', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 9, 'F','brown', 'funny', 'false');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 4, 'F', 'brown','happy', 'true');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, 'M','brown', 'silly', 'false');
